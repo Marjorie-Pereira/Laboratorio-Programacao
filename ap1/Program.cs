e1670d4e5a2f9c0a5cc90986b60f34160a69f556
Marjorie-Pereira <marjorie300904@gmail.com>
2023-09-27 10:59:49 -0300

    novas alteracoes

--- ap1/Program.cs
+++ ap1/Program.cs
@@ -1,83 +1,78 @@
-﻿// AP1: CALCULANDO IMC
+﻿using System;
 
-string? resp;
+namespace ap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULANDO IMC");
 
-string fxEtaria;
+            Relatorio();
+            
+        }
 
-string classificaImc;
+        static void Imc(double altura, double peso)
+        {
 
-do
-{   
-    // Guardando os dados
-    Console.WriteLine("Digite seu nome:");
-    string? nome = Console.ReadLine();
-    Console.WriteLine("Digite sua idade");
-    int idade = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Digite sua altura:");
-    double altura = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Digite seu peso");
-    double peso = Convert.ToDouble(Console.ReadLine());
+            
 
-    double imc = peso / (altura * altura);
-    
-    // classificacao do imc
-    if (imc < 18.5)
-    {
-        classificaImc = "abaixo do peso.";
-    }
-    else if (imc >= 18.6 && imc <= 24.9)
-    {
-        classificaImc = "com peso ideal.";
-    }
-    else if (imc >= 25 && imc <= 29.9)
-    {
-        classificaImc = "levemente acima do peso.";
-    }
-    else if (imc >= 30 && imc <= 34.9)
-    {
-        classificaImc = "com obesidade grau I";
-    }
-    else if (imc >= 35 && imc <= 39.9)
-    {
-        classificaImc = "com obesidade grau II";
-    }
-    else
-    {
-        classificaImc = "com obesidade grau III";
-    }
-    // verificando faixa etária
-    if (idade <= 12)
-    {
-        fxEtaria = "Criança";
-    }
-    else if (idade >= 13 && idade <= 19)
-    {
-        fxEtaria = "Adolescente";
-    }
-    else if (idade >= 20 && idade <= 60)
-    {
-        fxEtaria = "Adulto";
-    }
-    else
-    {
-        fxEtaria = "Idoso";
-    }
+            double imc = peso / (altura * altura);
 
-    // gerando relatório
-    Console.WriteLine("Nome: " + nome);
-    Console.WriteLine($"Idade: {idade} anos.");
-    Console.WriteLine("Faixa etária: " + fxEtaria);
-    Console.WriteLine("IMC: " + (int)imc);
-    Console.WriteLine("Você está " + classificaImc);
+            
+
+            if (imc <= 18.6)
+            {
+               Console.WriteLine($"IMC: {(int)imc}, abaixo do peso.");
+            }
+            else if (imc > 18.9 )
+            {
+                Console.WriteLine($"IMC: {(int)imc}, Indefinido.");
+            }
+
+        }
+
+        static void FaixaEtaria(int? idade)
+        {
+             if (idade <= 12)
+                {
+                     Console.WriteLine($"Faixa etária: {idade} anos, criança");
+                }
+                else if (idade >= 13 && idade <= 19)
+                {
+                     Console.WriteLine($"Faixa etária: {idade} anos, adolescente");
+                }
+                else if (idade >= 20 && idade <= 60)
+                {
+                    Console.WriteLine($"Faixa etária: {idade} anos, adulto");
+                }
+                else
+                {
+                    Console.WriteLine($"Faixa etária: {idade} anos, idoso");
+                }
 
 
-    // Solicitando resposta ao usuario
-    Console.WriteLine("\n Deseja fazer um novo relatório? [S/N]");
-    resp = Console.ReadLine();
-    resp = resp?.ToLower();
-    
 
+        }
+
+        static void Relatorio()
+        {
+            Console.WriteLine("Digite seu nome:");
+            string? nome = Console.ReadLine();
+            Console.WriteLine("Informe sua idade:");
+            int? idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe sua altura");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe seu peso");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Gerando relatório...");
+            Console.WriteLine("");
+            Console.WriteLine($"NOME: {nome}");
+            FaixaEtaria(idade);
+            Imc(altura, peso);
+            Console.WriteLine("");
+        }
+    }
 }
-while(resp == "s"); //loop baseado na resposta do usuario.
-
-Console.WriteLine("Encerrando...");
